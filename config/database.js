4bda7c4651ff5f6230f75c412e960c2552e30baa
Kavitha Thanigaivelan <kavithan98@gmail.com>
2018-08-05 10:00:49 -0700

    added prod db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
-if(process.env.NODE_ENV === 'production'){
-  module.exports ={mongoURI:'mongodb://kavi:Srilakshmi7@ds133920.mlab.com:33920/tcprod'}
-}else {
-  module.exports={mongoURI:'mongodb://localhost/tc-test'}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI: 'mongodb://kavitha:Srilakshmi7@ds113522.mlab.com:13522/tcprod'
+  };
+} else {
+  module.exports = { mongoURI: 'mongodb://localhost/tc-test' };
 }
--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
-// Load routes
-// const { UserPlan } = require('./models/userPlan');
-// const userplans = require('./userplanRouter');
 
 const db = require('./config/database');
 // Map global promise - get rid of warning
@@ -29,19 +26,6 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
-// Use routes
-// app.use('/userplan', userplans);
-//
-//
-// app.use('*', function (req, res) {
-//   res.status(404).json({ message: 'Not Found' });
-// });
-
-// //Server setup
-// const port = process.env.PORT || 3090;
-// const server = http.createServer(app);
-// server.listen(port);
-// console.log("Server listening on port: ",port);
 
 //Creating server object to use in runServer and closeServer, value will be assigned once the runServer runServer
 let server;
